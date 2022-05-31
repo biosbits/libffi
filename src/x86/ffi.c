dec9c9aa8951025db9ef921f21a3a196b0fff967
Ani Sinha <ani@anisinha.ca>
2022-05-31 20:50:47 +0530

    fix implicit fallthrough gcc warnings
    This patch addresses implicit fallthrough warnings on the
    newer gcc compiler. Proper comment to silence the warning or appropriate comments
    have been added in the case statements.
    
    Signed-off-by: Ani Sinha <ani@anisinha.ca>

--- src/x86/ffi.c
+++ src/x86/ffi.c
@@ -522,6 +522,7 @@ ffi_prep_closure_loc (ffi_closure* closure,
     case FFI_REGISTER:
       dest = ffi_closure_REGISTER;
       op = 0x68;  /* pushl imm */
+      // fall through
     default:
       return FFI_BAD_ABI;
     }
